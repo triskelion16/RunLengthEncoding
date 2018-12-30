f2cfc0c848000fd5e8c0ba5cce7f60446adeb60a
triskelion16 <triskelion.work@gmail.com>
2018-12-30 15:50:12 +0100

    [ADD] encoding method

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,25 +2,53 @@ package application;
 
 public class Controller {
 
+	//******** Encode *****************************************
 	public static String encode(String txt) {
-		String result = "";
 		StringBuffer sb = new StringBuffer();
+		String str = "";
 		int count = 1;
 		char separator = ',';
-		
-		if(txt.length() > 1) {
-					} else {
-			sb.append(txt);
-			sb.append(count);
+		char current = txt.charAt(0);
+
+		if (txt.length() > 1) { // jeśli ciąg znaków jest większy niż 1
+
+			for (int next = 1; next < txt.length(); next++) {
+				if (current == txt.charAt(next)) { // jężeli następny element jest równy z poprzednim
+					count++;
+					current = txt.charAt(next);
+
+					if (next == txt.length() - 1) { // jeżeli ostatni i przed ostatni element są równe muszą zostać dodane do sb
+						str = "" + current + count + separator;
+						sb.append(str);
+					}
+				} else { // jeżeli sąsiednie elementy nie są równe
+					
+					if (count > 1) { // jeżeli było wicej niż jeden taki sam element
+						str = "" + current + count + separator;
+						sb.append(str);
+						count = 1;
+						current = txt.charAt(next);
+					} else { // dodawanie pojedyńczych elementów
+						str = "" + current + count + separator;
+						sb.append(str);
+						current = txt.charAt(next);
+					}
+					if (next == txt.length() - 1) { // dodane do sb ostatniego pojedyńczego elementu
+						str = "" + current + count + separator;
+						sb.append(str);
+					}
+				}
+			}
+
+		} else {
+			str = "" + current + count;
+			sb.append(str);
 		}
-		
-		
-		result = sb.toString();
-		
-		System.out.println(result);
-		return result;
+
+		return sb.toString();
 	}
 
+	//******** Decode *****************************************
 	public static String decode(String txt) {
 
 		return null;
